6d6160c409b7a4d6a7dbe4f70fdf1fe792b58619
Jan ONDREJ (SAL) <ondrejj(at)salstar.sk>
2018-02-10 18:48:19 +0100

    Icon buttons fixes.

--- icons/mkstyle.sh
+++ icons/mkstyle.sh
@@ -7,7 +7,7 @@ ARGS="--create-groups=true \
 
 scour() {
   python /usr/share/inkscape/extensions/scour.inkscape.py "$@" \
-    | sed -e 's/#000/rgb(0,0,0)/g' -e 's/#fff/rgb(255,255,255)/g' \
+    | sed -e 's/#000/black/g' -e 's/#fff/white/g' \
           -e 's/#/%23/g' -e 's/</%3C/g' -e 's/>/%3E/g' -e "s/\"/'/g" \
     #| base64 -w 0
 }
@@ -15,7 +15,7 @@ scour() {
 cd `dirname $0`
 for svg in *.svg; do
   name="${svg%.svg}"
-  echo "div.trafgrapher button.b_$name div {"
+  echo "div.trafgrapher button.b_$name {"
   echo "  background-image: url(\"data:image/svg+xml,`scour $ARGS $svg`\");"
   echo "}"
 done
