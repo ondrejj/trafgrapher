db688e764ba25077a9429a98c82fa5eeb4225f0b
Jan ONDREJ (SAL) <ondrejj(at)salstar.sk>
2018-02-11 10:30:21 +0100

    Programmed svg images.

--- icons/mkstyle.sh
+++ icons/mkstyle.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
 
-ARGS="--create-groups=true \
-  --enable-id-stripping=true --enable-comment-stripping=true \
-  --remove-metadata=true --strip-xml-prolog=true --enable-viewboxing=true \
-  --line-breaks=false"
-
-scour() {
-  python /usr/share/inkscape/extensions/scour.inkscape.py "$@" \
-    | sed -e 's/#000/black/g' -e 's/#fff/white/g' \
-          -e 's/#/%23/g' -e 's/</%3C/g' -e 's/>/%3E/g' -e "s/\"/'/g" \
-    #| base64 -w 0
+optimize() {
+  tr -d '\n' < "$1" | sed -e 's/ \+/ /g' -e 's/> \+</></g' \
+    -e 's/#/%23/g' -e 's/</%3C/g' -e 's/>/%3E/g' -e "s/\"/'/g"
 }
 
 cd `dirname $0`
 for svg in *.svg; do
   name="${svg%.svg}"
   echo "div.trafgrapher button.b_$name {"
-  echo "  background-image: url(\"data:image/svg+xml,`scour $ARGS $svg`\");"
+  echo "  background-image: url(\"data:image/svg+xml,`optimize $svg`\");"
   echo "}"
 done
